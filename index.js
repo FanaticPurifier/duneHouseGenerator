c938cec86d19f2ce4e530bbc66318367b63083c3
Paul O'Neill <pauloneill08@outlook.com>
2022-05-09 14:37:57 +0100

    Add json files for house templates

---
+++ index.js
@@ -0,0 +1,8 @@
+const names = require('./templates/names.json')
+const homeworld = require('./templates/homeworld.json')
+const domain = require('./templates/domain.json')
+
+const buildHouse = (names, domain, homeworld) => {
+    let house = {}
+
+}
