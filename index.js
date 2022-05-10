77ed17ac8fd476ed4e0d54d624129d7ee0a18943
Paul O'Neill <pauloneill08@outlook.com>
2022-05-10 15:44:50 +0100

    refactor: added getRandomElement function

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const names = require('./templates/names.json')
 const homeworld = require('./templates/homeworld.json')
 const domain = require('./templates/domain.json')
+const a = require('indefinite')
+
+const getRandomElement = (array) => {
+    return array[Math.floor(Math.random() * array.length)]
+}
 
 const buildHouse = (names, domain, homeworld) => {
     let house = {
-        "name": names[Math.floor(Math.random() * names.length)],
-        "homeworldName": homeworld.planetName[Math.floor(Math.random() * homeworld.planetName.length)],
-        "homeworldType": homeworld.terrain[Math.floor(Math.random() * homeworld.terrain.length)],
-        "primaryDomain": domain[Math.floor(Math.random() * domain.length)],
+        "name": getRandomElement(names),
+        "homeworldName": getRandomElement(homeworld.planetName),
+        "homeworldType": getRandomElement(homeworld.terrain),
+        "primaryDomain": getRandomElement(domain),
         // The following setters reduce the primaryDomain object down to single randomly chosen Areas of Expertise and Specialisation
         setAreaOfExpertise(){
-            house.primaryDomain.areaOfExpertise = house.primaryDomain.areaOfExpertise[Math.floor(Math.random() * house.primaryDomain.areaOfExpertise.length)]
+            house.primaryDomain.areaOfExpertise = getRandomElement(house.primaryDomain.areaOfExpertise)
         },
         setSpecialisation(){
-            house.primaryDomain.areaOfExpertise.specialisation = house.primaryDomain.areaOfExpertise.specialisation[Math.floor(Math.random() * house.primaryDomain.areaOfExpertise.specialisation.length)]
+            house.primaryDomain.areaOfExpertise.specialisation = getRandomElement(house.primaryDomain.areaOfExpertise.specialisation)
         }
     }
     
@@ -21,8 +26,8 @@ const buildHouse = (names, domain, homeworld) => {
     house.setSpecialisation(house);
     const writeDescription = (house) => {
       console.log(`${house.name} from the planet ${house.homeworldName}`);
-      console.log(`${house.homeworldName} is a ${house.homeworldType} planet`);
-      console.log(`${house.name} is known across the galaxy for it's ${house.primaryDomain.domainName}. They are experts in ${house.primaryDomain.domainName} ${house.primaryDomain.areaOfExpertise.expertiseName}.`);
+      console.log(`${house.homeworldName} is ` + a(`${house.homeworldType} planet`));
+      console.log(`${house.name} is known across the galaxy for its ${house.primaryDomain.domainName}. They are experts in ${house.primaryDomain.domainName} ${house.primaryDomain.areaOfExpertise.expertiseName}.`);
       console.log(`For example, ${house.primaryDomain.areaOfExpertise.specialisation}.`);
     };
     
